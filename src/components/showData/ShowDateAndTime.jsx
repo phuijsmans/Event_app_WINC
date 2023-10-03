6f7588c9c6382563ea238e2c7feb2595501df9b1
phuijsmans <huijsmanspieter@gmail.com>
2023-10-03 16:43:54 +0200

    Removed unnecessary code

--- src/components/showData/ShowDateAndTime.jsx
+++ src/components/showData/ShowDateAndTime.jsx
@@ -4,9 +4,7 @@ export const ShowDateAndTime = ({ date }) => {
   const getDateAndTime = (date) => {
     const getDate = date.slice(0, 10);
     const getTime = date.slice(11, 16);
-    console.log(getDate);
 
-    // return [getDate, getTime];
     return (
       <Flex gap="1em">
         <Text>D: {getDate}</Text>
